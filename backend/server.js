25d1b1a78cb8a707b869e4b353ef7d9fab8990a5
Dev Gupta <devgupta8770@gmail.com>
2024-09-25 17:04:04 +0530

    cors solved

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ const server = app.listen(5000, console.log(`server started on port ${PORT}`.blu
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "https://devgupta0513.github.io/LinkUs-minor-1",
+        origin: "https://devgupta0513.github.io",
 
     },
 });
