a729906e33e671bb30ac842dd4909ad855a59b14
Dev Gupta <devgupta8770@gmail.com>
2025-05-14 00:16:30 +0530

    side drwawer bug fixed

--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -58,7 +58,7 @@ const SideDrawer = () => {
 
             const config = {
                 headers: {
-                    Authorization: `Bearer ${user.token}}`,
+                    Authorization: `Bearer ${user.token}`,
                 },
             };
 
